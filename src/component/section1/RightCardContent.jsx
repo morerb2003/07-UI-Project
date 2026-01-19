ea2be446717c023405e11f293bfca75355a7f3e6
Rohit More <morerb2003@gmail.com>
2026-01-19 19:40:44 +0530

    Updated UI components and styles

--- src/component/section1/RightCardContent.jsx
+++ src/component/section1/RightCardContent.jsx
@@ -4,7 +4,7 @@ const RightCardContent = () => {
             <div className='absolute top-0 left-0 h-full w-full p-8 flex  flex-col justify-between '>
                                 <h2 className='bg-white text-2xl font-semibold rounded-full h-14 w-14 flex justify-center items-center'>1</h2>
                                 <div>
-                                        <p className='text-xl leading-snug text-white mb-10  '>loren ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+                                        <p className='text-xl leading-snug text-white mb-10  '>loren ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum......</p>
 
                                         <div className='flex justify-between'>
                                                 <button className='bg-blue-600 text-white font-medium px-8 py-2 rounded-full'>Satisfied</button>
