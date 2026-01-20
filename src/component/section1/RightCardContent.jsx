d57c6e23db4aeef2d1797169683dba7b0dca5b25
Rohit More <morerb2003@gmail.com>
2026-01-20 23:36:53 +0530

    addend

--- src/component/section1/RightCardContent.jsx
+++ src/component/section1/RightCardContent.jsx
@@ -7,7 +7,7 @@ const RightCardContent = (props) => {
             <div className='absolute top-0 left-0 h-full w-full p-8 flex flex-col justify-between'>
                 <h2 className='bg-white text-1xl font-semibold rounded-full h-10 w-10 flex justify-center items-center'>{props.id + 1}</h2>
                 <div>
-                    <p className='text-sm leading-snug text-white mb-10'>loren ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum</p>
+                    <p className='text-sm leading-snug text-white mb-10'>loren ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
                     <div className='flex justify-between'>
                         <button className={`${props.color} text-white font-medium px-8 py-2 rounded-full`}>{props.tag}</button>
                         <button className={`${props.color} text-white font-medium px-3 py-2 rounded-full`}><i className='ri-arrow-right-line'></i></button>
